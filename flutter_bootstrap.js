249e88dbc4c021e2a1a65d59e8f7648040ac6757
FluffingNewt <95949622+FluffingNewt@users.noreply.github.com>
2025-02-03 15:06:41 +0000

    Deploying to gh-pages from @ FluffingNewt/ctww@3e887dc4bfcf5cf74906b0eb4ca3a5d17f2a2776 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2247162628"
+    serviceWorkerVersion: "1628084286"
   }
 });
