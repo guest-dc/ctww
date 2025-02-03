380c96fa5d144ade9e49857f4596edaf25668721
Davis Guest <davisdevice@gmail.com>
2025-02-03 11:43:07 -0500

    added lesson_bar to story walkthrough and anatomy lab pages

--- ctww/lib/pages/story_walk_page.dart
+++ ctww/lib/pages/story_walk_page.dart
@@ -1,18 +1,47 @@
+import 'package:ctww/utils/colors.dart';
 import 'package:flutter/material.dart';
-
-//import '../utils/colors.dart';
 import '../utils/nav_bar.dart';
+import '../utils/lesson_bar.dart';
 
-class StoryWalkPage extends StatelessWidget {
+class StoryWalkPage extends StatefulWidget {
+  @override
+  _StoryWalkPageState createState() => _StoryWalkPageState();
+}
+
+class _StoryWalkPageState extends State<StoryWalkPage> {
+  bool _isLessonBarVisible = false;
+
+  void _toggleLessonBar() {
+    setState(() {
+      _isLessonBarVisible = !_isLessonBarVisible;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: NavBar(),
-      body: Center(
-        child: Text(
-          'Story Walkthrough stuff goes here',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Stack(
+        children: [
+          Center(
+            child: Text(
+              'Story Walkthrough stuff goes here',
+              style: TextStyle(fontSize: 20),
+            ),
+          ),
+          Positioned(
+            right: 0,
+            top: 0,
+            bottom: 0,
+            child: LessonBar(isVisible: _isLessonBarVisible, toggleVisibility: _toggleLessonBar),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _toggleLessonBar,
+        backgroundColor: colorGOLD,
+        foregroundColor: colorWHITE,
+        child: Icon(Icons.menu_book),
       ),
     );
   }
--- ctww/lib/utils/colors.dart
+++ ctww/lib/utils/colors.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const Color colorRED = Color.fromARGB(255, 215, 0, 0);
 const Color colorGOLD = Color.fromARGB(255, 204, 153, 2);
+const Color colorWHITE = Color.fromARGB(255, 255, 255, 255);
