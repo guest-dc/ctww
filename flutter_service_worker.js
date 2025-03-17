231a259dab375c5b7c1a58df268ff2946890c892
Maryam-maryam <125651238+Maryam-maryam@users.noreply.github.com>
2025-03-17 10:58:45 +0000

    Deploying to gh-pages from @ guest-dc/ctww@4c0daaeef23524c8188d6ed83ab27058258efb0d 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "manifest.json": "75791017a43e391e38e91741d60f102e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "182f77164ade5b7a7c2c53c120268782"};
+"flutter_bootstrap.js": "e364d6edd0298e7a41950de383c182e6"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
